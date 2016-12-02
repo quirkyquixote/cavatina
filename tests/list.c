ae6a0f6de8844a80839afea594a2764138492865
Luis Sanz <luis.sanz@gmail.com>
2016-12-02 14:01:21 +0100

    better list test
    A more exhaustive test that does more operations on the list, including
    emptying it in different ways, and checks both manually and using
    kv_foreach.

--- tests/list.c
+++ tests/list.c
@@ -8,41 +8,107 @@
 
 #include "../cavatina.h"
 
-struct node {
-	struct kv_node node;
-	int data;
-};
-
 int main(int argc, char *argv[])
 {
 	struct kv_list list;
-	struct node nodes[10];
+	struct kv_node nodes[10];
 	struct kv_node *iter;
-	int i;
+	int i, j;
 
-	for (i = 0; i < 10; ++i)
-		nodes[i].data = i;
-
+	/* Create empty list */
 	kv_list_init(&list);
-	for (i = 0; i < 10; ++i)
-		kv_list_add(&list, &nodes[i].node);
-	kv_foreach(iter, &list)
-		printf("%d\n", ((struct node *)iter)->data);
-	for (i = 1; i < 10; i += 2)
-		kv_list_remove(&list, &nodes[i].node);
-	kv_foreach(iter, &list)
-		printf("%d\n", ((struct node *)iter)->data);
+	assert(list.first == NULL);
+	assert(list.last == NULL);
 
-	kv_list_init(&list);
-	for (i = 0; i < 10; ++i)
-		kv_list_add(&list, &nodes[i].node);
-	kv_foreach(iter, &list)
-		printf("%d\n", ((struct node *)iter)->data);
+	/* Fill list with all nodes */
+	for (i = 0; i < 10; ++i) {
+		kv_list_add(&list, &nodes[i]);
+		/* Check manually */
+		assert(list.first == &nodes[0]);
+		assert(list.last == &nodes[i]);
+		iter = list.first;
+		for (j = 0; j <= i; ++j) {
+			assert(iter == &nodes[j]);
+			iter = iter->next;
+		}
+		assert(iter == NULL);
+		/* Check with kv_foreach */
+		j = 0;
+		kv_foreach(iter, &list) {
+			assert(iter == &nodes[j]);
+			++j;
+		}
+	}
+
+	/* Remove even nodes */
 	for (i = 0; i < 10; i += 2)
-		kv_list_remove(&list, &nodes[i].node);
-	kv_foreach(iter, &list)
-		printf("%d\n", ((struct node *)iter)->data);
+		kv_list_remove(&list, &nodes[i]);
+	/* Check manually */
+	assert(list.first == &nodes[1]);
+	assert(list.last == &nodes[9]);
+	iter = list.first;
+	for (i = 1; i < 10; i += 2) {
+		assert(iter == &nodes[i]);
+		iter = iter->next;
+	}
+	assert(iter == NULL);
+	/* Check with kv_foreach */
+	i = 1;
+	kv_foreach(iter, &list) {
+		assert(iter == &nodes[i]);
+		i += 2;
+	}
 
+	/* Remove odd nodes */
+	for (i = 1; i < 10; i += 2)
+		kv_list_remove(&list, &nodes[i]);
+	assert(list.first == NULL);
+	assert(list.last == NULL);
+
+	/* Fill again the list */
+	for (i = 0; i < 10; ++i) {
+		kv_list_add(&list, &nodes[i]);
+		/* Check manually */
+		assert(list.first == &nodes[0]);
+		assert(list.last == &nodes[i]);
+		iter = list.first;
+		for (j = 0; j <= i; ++j) {
+			assert(iter == &nodes[j]);
+			iter = iter->next;
+		}
+		assert(iter == NULL);
+		/* Check with kv_foreach */
+		j = 0;
+		kv_foreach(iter, &list) {
+			assert(iter == &nodes[j]);
+			++j;
+		}
+	}
+
+	/* Remove even nodes */
+	for (i = 1; i < 10; i += 2)
+		kv_list_remove(&list, &nodes[i]);
+	/* Check manually */
+	assert(list.first == &nodes[0]);
+	assert(list.last == &nodes[8]);
+	iter = list.first;
+	for (i = 0; i < 10; i += 2) {
+		assert(iter == &nodes[i]);
+		iter = iter->next;
+	}
+	assert(iter == NULL);
+	/* Check with kv_foreach */
+	i = 0;
+	kv_foreach(iter, &list) {
+		assert(iter == &nodes[i]);
+		i += 2;
+	}
+
+	/* Remove odd nodes */
+	for (i = 0; i < 10; i += 2)
+		kv_list_remove(&list, &nodes[i]);
+	assert(list.first == NULL);
+	assert(list.last == NULL);
 	return 0;
 }
 
